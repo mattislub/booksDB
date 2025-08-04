8491470220c1a7c176b6e0173c96eba2d4bb8690
mattislub <166994172+mattislub@users.noreply.github.com>
2025-08-05 01:04:33 +0300

    Use unified App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './App.jsx';
 
 test('renders Catalog component on home page', () => {
   render(
